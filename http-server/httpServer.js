6bd6cfa4be056d826ac3f5685e3b41c103d57d45
RavirajNavale-Github <165894463+RavirajNavale-Github@users.noreply.github.com>
2024-05-08 18:42:10 +0530

    some updates

--- http-server/httpServer.js
+++ http-server/httpServer.js
@@ -6,11 +6,13 @@ const server = http.createServer((req, res) => {
   if (req.url === "/users" && req.method === "GET") {
     // Define JSON API endpoint URL
     const apiUrl = "https://jsonplaceholder.typicode.com/users";
+    
+    console.log(req.url); //checking the requested method
 
     // Send HTTP response headers
     res.writeHead(200, { "Content-Type": "application/json" });
 
-    // Fetch user data from JSON API
+    // Fetch user data from API
     https
       .get(apiUrl, (apiRes) => {
         let data = "";
@@ -24,7 +26,10 @@ const server = http.createServer((req, res) => {
           // Parse JSON data and send response
           const users = JSON.parse(data); //convert to JS object
           res.end(JSON.stringify(users)); //convert to string
+          
         });
+
+        // console.log(res)
       })
       .on("error", (err) => {
         console.error("Error fetching user data:", err);
@@ -33,16 +38,21 @@ const server = http.createServer((req, res) => {
   } else if (req.url === "/users" && req.method === "POST") {
     let data = "";
 
+    // console.log(req.method); //checking the requested method
+    // console.log(req.url); //checking the requested url
+
     //fired when added content in data
     req.on("data", (chunk) => {
       data += chunk;
     });
 
+    //fires at the end of resopnse
     req.on("end", () => {
       const postData = JSON.parse(data);
       //here logic for add data in api by POST method
       res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end(JSON.stringify(postData));
+      // res.end(JSON.stringify(postData));
+      res.end(JSON.stringify("Data is POSTED"));
     });
   } else if (req.url.startsWith("/users/") && req.method === "PUT") {
     //get user Id
@@ -56,15 +66,17 @@ const server = http.createServer((req, res) => {
 
     req.on("end", () => {
       const updateData = JSON.parse(data);
-
+      //here logic for add data in api by PUT method
       res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end(`User ${userId} updated successfully: ${JSON.stringify(updateData)}`);
+      res.end(`User with ID: ${userId} updated successfully: ${JSON.stringify(updateData)}`);
     });
   } else {
     //Other request
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
   }
+
+  
 });
 
 // Specify the port to listen on
